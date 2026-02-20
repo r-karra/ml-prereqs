30b934dca3260391330c4f16f5d39b788946e798
Rajesh Karra <rajeshkumarkarra@outlook.com>
2026-02-20 10:50:44 +0000

    Build to docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ml-prereqs/' // For GitHub Pages deployment
+  base: '/ml-prereqs/', // For GitHub Pages deployment
+  build: {
+    outDir: 'docs'
+  }
 })
